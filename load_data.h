8d286b7f5e3f4f2bdc02da54e0fcbd0f516a5063
liujiejie <654783042@qq.com>
2015-09-02 15:34:13 +0800

    update

--- load_data.h
+++ load_data.h
@@ -15,6 +15,7 @@ class LoadData
 public:
     static long LoadSuang()
 	{
+		S_suang* suang_unit = NULL;
 		char buffer[50];
 		for(int i = 0; i < SUANG_FILE_COUNT; i++)
 		{
@@ -29,7 +30,23 @@ public:
 			while (! in.eof() )  
 			{
 			   in.getline (buffer,50);
-			   std::cout << buffer << std::endl;  
+			   if (strlen(buffer) < 10)
+			   {
+					continue;
+			   }
+			   suang_unit = new S_suang;
+			   suang_unit->number_ = atoi(&buffer[0]);
+			   suang_unit->red_[0] = atoi(&buffer[11]);
+			   suang_unit->red_[1] = atoi(&buffer[14]);
+			   suang_unit->red_[2] = atoi(&buffer[17]);
+			   suang_unit->red_[3] = atoi(&buffer[20]);
+			   suang_unit->red_[4] = atoi(&buffer[23]);
+			   suang_unit->red_[5] = atoi(&buffer[26]);
+			   suang_unit->blue_   = atoi(&buffer[29]);
+			   suang_unit->date_   = atoi(&buffer[35]) * 10000 +
+									 atoi(&buffer[40]) * 100 +
+									 atoi(&buffer[43]);
+			   suang_unit->Print();
 			}
 			
 			in.close();
--- struct.h
+++ struct.h
@@ -1,16 +1,49 @@
 #ifndef _HEHE_STRUCT_H_
 #define _HEHE_STRUCT_H_
 
+#include <iostream>
+#include <iomanip>
+
 namespace hehe
 {
 //0~5 红球
 //6   篮球
 struct S_suang
 {
-int 		red_[5];
-int			blue_;
-std::string date_;
-std::string number_;
+	int red_[6];
+	int	blue_;
+	int date_;
+	int number_;
+	
+	void Print()
+	{
+		std::cout.flags(std::ios::right); //右对齐
+		std::cout.width(2);
+		std::cout.fill(' ');
+		std::cout << "\033[31;1m"    << std::setw(2)
+				  << red_[0] << "  " << std::setw(2)
+				  << red_[1] << "  " << std::setw(2)
+				  << red_[2] << "  " << std::setw(2)
+				  << red_[3] << "  " << std::setw(2)
+				  << red_[4] << "  " << std::setw(2)
+				  << red_[5] << "  " << "\033[31;4m" << std::setw(2)
+		          << blue_   << "  " << "\033[31;0m"
+				  << date_   << "  "
+				  << number_ << std::endl;
+	};
+	
+	bool operator < (const S_suang& unit)const
+	{
+		if (date_ < unit.date_)
+		{
+			return true;
+		}
+		
+		if (date_ > unit.date_)
+		{
+			return false;
+		}
+	}
 };
 
 }
